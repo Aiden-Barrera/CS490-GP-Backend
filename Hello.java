4fe2e062a10d86859857f2472c753c6c440dc760
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-02-16 20:16:28 -0500

    Added Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] argc){
+       System.out.println(Math.random());
+       System.out.println(Math.random());
+       System.out.println(Math.random());
+    }
+}
