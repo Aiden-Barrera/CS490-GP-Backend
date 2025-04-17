2111ac83d43f1fd85316ded8dc1cc1c0e3139f82
FardeenI <fi43@njit.edu>
2025-04-16 23:43:08 -0400

    Create API Key authorization-Check middleware

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ import { addPatientDoc, createAppointment, createChatMsg, createChatroom, create
 
 import cors from 'cors'
 import multer from 'multer'
+import dotenv from 'dotenv'
+dotenv.config()
 
 //import socket from 'socket.io'
 /*
@@ -37,12 +39,29 @@ app.use(cors())
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
-  })
+})
 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+
+const apiKeyMiddleware = (req, res, next) => {
+    const apiKey = req.headers['x-api-key']; // Or req.query.apiKey if you prefer query parameters
+  
+    if (!apiKey) {
+      return res.status(401).json({ message: 'API key required' });
+    }
+  
+    // In real applications, validate the API key against a database or environment variable
+    if (apiKey !== process.env.API_KEY) {
+      return res.status(403).json({ message: 'Invalid API key' });
+    }
+  
+    next(); // Proceed to the next middleware or route handler
+};
+
+// app.use(apiKeyMiddleware)
 
 const store = multer.diskStorage({
     destination: (req, file, cb) => { //where to store (folder name ExerciseBankImages)
