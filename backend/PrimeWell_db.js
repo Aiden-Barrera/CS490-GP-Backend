8796c8f998ecdee0c94651aaea010f5e44ef2bcb
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-04-16 21:47:24 -0400

    Updated getApptRequest to include Request_Status

--- backend/PrimeWell_db.js
+++ backend/PrimeWell_db.js
@@ -359,8 +359,8 @@ export async function getTimeslot(Doctor_ID, Appt_Date, Appt_Time) {
 export async function getApptRequest(id) {
     try {
     const [resultRows] = await pool.query(`
-        SELECT patientbase.First_name, patientbase.last_name, Requests.Patient_ID, Requests.Doctor_ID, Requests.Appt_Date, Requests.Appt_Time, Requests.Tier
-        FROM requests INNER JOIN patientbase ON patientbase.Patient_ID = requests.Patient_ID
+        SELECT patientbase.First_name, patientbase.last_name, Requests.Patient_ID, Requests.Doctor_ID, Requests.Appt_Date,
+        Requests.Appt_Time, Requests.Tier, Requests.Request_Status FROM requests INNER JOIN patientbase ON patientbase.Patient_ID = requests.Patient_ID
         WHERE requests.Doctor_ID = ?;`, [id]) 
         return resultRows
     }
