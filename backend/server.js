9ba05974ab3789e121369e53fca487f7c08c0328
FardeenI <fi43@njit.edu>
2025-04-22 20:44:44 -0400

    Nothin crazy

--- backend/server.js
+++ backend/server.js
@@ -125,20 +125,6 @@ io.on('connection', (socket) => {
 //</script>
 */
 
-const apiKeyMiddleware = (req, res, next) => {
-    const apiKey = req.headers['x-api-key']; // Or req.query.apiKey if you prefer query parameters
-
-    if (!apiKey) {
-      return res.status(401).json({ message: 'API key required' });
-    }
-
-    // In real applications, validate the API key against a database or environment variable
-    if (apiKey !== process.env.API_KEY) {
-      return res.status(403).json({ message: 'Invalid API key' });
-    }
-
-    next(); // Proceed to the next middleware or route handler
-};
 
 //GET DATA ----------------------------------------------------------------------------------------------
 
