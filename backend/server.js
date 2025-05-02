11429f01c00a75e10b9cc74722acdbbef065a4c8
Aiden-Barrera <aidenjayce28@gmail.com>
2025-05-01 23:13:58 -0400

    Removed CRS

--- backend/server.js
+++ backend/server.js
@@ -62,12 +62,12 @@ app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
 })
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// Remove any existing Content-Security-Policy header
 app.use((req, res, next) => {
     res.removeHeader('Content-Security-Policy');
     next();
-  });
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Remove any existing Content-Security-Policy header
   
 
 const server = http.createServer(app)
