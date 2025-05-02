b8f4ed318a457d6831b25f7274e16c1c9e90a815
Aiden-Barrera <aidenjayce28@gmail.com>
2025-05-02 17:32:15 -0400

    Updated Rabbitmq.js to connect to the cloud server hosting a rabbitmq instance

--- backend/rabbitmq.js
+++ backend/rabbitmq.js
@@ -4,8 +4,10 @@ import {
     createPayment,
     getPrescriptionWithNamesById
 } from './PrimeWell_db.js';
+dotenv.config()
 
-const RABBITMQ_URL = 'amqp://localhost';
+
+const RABBITMQ_URL = process.env.RABBITMQ_URL;
 const EXCHANGE_NAME = 'prescriptions_exchange';
 
 async function sendPrescription(pharmacyName, prescriptionData) {
