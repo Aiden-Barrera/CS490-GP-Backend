ada648f2f3febf99db0084dbd23ed016dac8bcd8
Aiden-Barrera <aidenjayce28@gmail.com>
2025-05-06 19:35:26 -0400

    Added console log for getDoctorSchedules

--- backend/PrimeWell_db.js
+++ backend/PrimeWell_db.js
@@ -139,7 +139,7 @@ export async function getDoctorSchedule(id, day, date) {
         console.log("Slots: ", slotsRow) 
         const [bookedRows] = await pool.query(`select appt_time from Appointments where doctor_id = ? and appt_date = ?`, [id, date])
         // Parse JSON string from MySQL
-        const fullSlots = JSON.parse(slotsRow[0].Slots);
+        const fullSlots = JSON.parse(slotsRow[0]?.Slots);
         const bookedSlots = bookedRows.map(row => row.appt_time);
         //console.log(bookedSlots)
         
